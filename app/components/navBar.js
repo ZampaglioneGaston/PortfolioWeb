f86bc32d5bd0d871cbd4acd2c23ec66d4971de4f
Your Name <gzampaglione@majorkeytech.com>
2023-06-21 23:20:07 -0300

    changed nav icon

--- app/components/navBar.js
+++ app/components/navBar.js
@@ -22,9 +22,10 @@ function NavegationBar(){
 
     
       <a type="a" href='#experience' className="flex items-center flex-col  justify-center px-5 border-gray-200 border-x hover:bg-gray-50 dark:hover:bg-gray-800 group dark:border-gray-600">
-        <svg className="w-6 h-6 mb-1 text-gray-700 dark:text-gray-400 group-hover:text-teal-600 dark:group-hover:text-teal-600" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-          <path d="M5 4a1 1 0 00-2 0v7.268a2 2 0 000 3.464V16a1 1 0 102 0v-1.268a2 2 0 000-3.464V4zM11 4a1 1 0 10-2 0v1.268a2 2 0 000 3.464V16a1 1 0 102 0V8.732a2 2 0 000-3.464V4zM16 3a1 1 0 011 1v7.268a2 2 0 010 3.464V16a1 1 0 11-2 0v-1.268a2 2 0 010-3.464V4a1 1 0 011-1z"></path>
-        </svg>
+      <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 mb-1 text-gray-700 dark:text-gray-400 group-hover:text-teal-600 dark:group-hover:text-teal-600" fill="currentColor" viewBox="0 0 20 20" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12a7.5 7.5 0 0015 0m-15 0a7.5 7.5 0 1115 0m-15 0H3m16.5 0H21m-1.5 0H12m-8.457 3.077l1.41-.513m14.095-5.13l1.41-.513M5.106 17.785l1.15-.964m11.49-9.642l1.149-.964M7.501 19.795l.75-1.3m7.5-12.99l.75-1.3m-6.063 16.658l.26-1.477m2.605-14.772l.26-1.477m0 17.726l-.26-1.477M10.698 4.614l-.26-1.477M16.5 19.794l-.75-1.299M7.5 4.205L12 12m6.894 5.785l-1.149-.964M6.256 7.178l-1.15-.964m15.352 8.864l-1.41-.513M4.954 9.435l-1.41-.514M12.002 12l-3.75 6.495" />
+</svg>
+
         <span className="text-sm text-gray-500 dark:text-gray-400 group-hover:text-teal-600 dark:group-hover:text-teal-600">Experiencies</span>
       </a>
 
